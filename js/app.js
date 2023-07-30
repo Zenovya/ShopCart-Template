c156ce87a2761048fa1942faff1fe8bab1136dc9
LuisLovo <joseluisl2003@gmail.com>
2023-07-30 00:23:30 -0600

    Finalizado

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 // variables
-const carts = document.querySelector('#carrito');
+const cart = document.querySelector('#carrito');
 const listCourse = document.querySelector('#lista-cursos');
 const containerCart = document.querySelector('#lista-carrito tbody');
 const btnClear = document.querySelector('#vaciar-carrito');
-let  items = [];
+let items = [];
 
 loadListeners()
 
-function addCourse(e){
+function addCourse(e) {
     e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
+    if (e.target.classList.contains('agregar-carrito')) {
         const selectCourse = e.target.parentElement.parentElement;
 
         data(selectCourse);
@@ -18,35 +18,56 @@ function addCourse(e){
 
 function loadListeners() {
     listCourse.addEventListener('click', addCourse);
+
+    cart.addEventListener('click', deleteCourse);
+
+    btnClear.addEventListener('click', () => {
+        items = []
+        clearHTML()
+    })
 }
 
 function data(course) {
     const info = {
-        id: course.querySelector('a').getAttribute('data-id'), 
+        id: course.querySelector('a').getAttribute('data-id'),
         title: course.querySelector('h4').textContent,
         image: course.querySelector('img').src,
         price: course.querySelector('.precio span').textContent,
         amount: 1
     }
 
-    items = [...items, info];
-    
+    const band = items.some(course => course.id === info.id);
+
+    if (band) {
+        const array = items.map(course => {
+            if (course.id === info.id) {
+                course.amount++;
+                return course;
+            } else {
+                return course;
+            }
+        })
+        items = [...array];
+    } else {
+        items = [...items, info];
+    }
+
     addCarts();
 }
 
-function addCarts(){
+function addCarts() {
     clearHTML();
-    items.forEach( course =>{
-        console.log(course)
+    items.forEach(course => {
+        const { image, title, price, amount, id } = course;
         const row = document.createElement('tr');
         row.innerHTML = `
             <td>    
-                <img src="${course.image}" width="150">
+                <img src="${image}" width="150">
             </td>
-            <td>${course.title}</td>
-            <td>${course.price}</td>
-            <td>${course.amount}</td>
-            <td><a href="#" class="borrar-curso" data-id="${course.id}"> X </a> </td>
+            <td>${title}</td>
+            <td>${price}</td>
+            <td>${amount}</td>
+            <td><a href="#" class="borrar-curso" data-id="${id}"> X </a> </td>
 
         `;
 
@@ -54,9 +75,20 @@ function addCarts(){
     })
 }
 
-function clearHTML(){
-    while(containerCart.firstChild){
-        containerCart.removeChild(containerCart.firstChild)
+function deleteCourse(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('borrar-curso')) {
+        const courseId = e.target.getAttribute('data-id');
+
+        items = items.filter(course => course.id !== courseId);
+
+        addCarts();
+
     }
 }
 
+function clearHTML() {
+    while (containerCart.firstChild) {
+        containerCart.removeChild(containerCart.firstChild)
+    }
+}
